6f2cdbee7da14d353a68bfdaed44a9304f72e877
Richard Bao <richardbao419@gmail.com>
2019-03-09 17:55:45 -0800

    update

--- besttest/js/home.js
+++ besttest/js/home.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
-    // $('#painting').html( "<span class='red'>Hello <b>Again</b></span>" );
-    $('#painting').load('lightbulb/index.html');
-});
+// $(document).ready(function(){
+//     $('#painting').load("lightbulb/index.html");
+// });
